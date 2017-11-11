d7b69988539d6f4ac81a1f1050cb6ad5cdf10702
toneloke <guerrerotonyjs@gmail.com>
2017-11-11 02:29:32 -0500

    api call working and cards displayed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Layout, Menu, Icon, Row } from 'antd'
+import SimpleForecast from './simpleForecast'
+const { Header, Content, Footer, Sider } = Layout
 
 class App extends Component {
-  render() {
+  state ={
+    city: '',
+    currentDay: {},
+    forecast: []
+  }
+
+  componentWillMount () {
+    console.log('fire')
+    fetch('http://api.openweathermap.org/data/2.5/forecast?q=NewYork,us&appid=06c3da063b27db6b0a0cdfdc00c928fb&units=imperial')
+    .then(res => res.json())
+    .then(weather => {
+      console.log(weather)
+      let city = weather.city.name
+      let currentDay = weather.list[0]
+      let forecast = weather.list
+      this.setState({city,currentDay, forecast})
+
+    })
+    .catch(err => console.log(err))
+  }
+  weatherBoxes(){
+    return this.state.forecast.map( day => <SimpleForecast day={day} />)
+  }
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Layout>
+        <Header style={{ background: '#fff', padding: 0 }}>
+          <Icon
+            className='trigger'
+            type={'cloud-o'}
+          />
+          GeoMe
+          <Icon
+            className='trigger'
+            type={'smile'}
+          />
+        </Header>
+        <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 260 }}>
+          <div style={{ background: '#ECECEC', padding: '30px' }}>
+            <Row gutter={5}>
+              { this.state.forecast.length > 0 ? this.weatherBoxes() : null}
+            </Row>
+          </div>
+        </Content>
+      </Layout>
+    )
   }
 }
 
-export default App;
+export default App
